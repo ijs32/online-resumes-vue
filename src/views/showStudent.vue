1ed7ca113da49c7f01977e6540bab25205bc01d0
Ian S <iansilber32@gmail.com>
2021-12-08 14:59:45 -0500

    makes vue app compatible with current backend teams api

--- src/views/showStudent.vue
+++ src/views/showStudent.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="students-show">
     <h1>First name: {{ student.first_name }}</h1>
+    <h3>Last name: {{ student.last_name }}</h3>
     <h3>email:{{ student.email }}</h3>
-    <h3>Twitter handle: {{ student.twitter_handle }}</h3>
+    <h3>Phone number: {{ student.phone_number }}</h3>
+    <h3>Short bio: {{ student.short_bio }}</h3>
+    <h3>Linkedin: {{ student.linkedin }}</h3>
+    <h3>Twitter handle: {{ student.twitter }}</h3>
+    <h3>Website: {{ student.website }}</h3>
+    <h3>Online resume: {{ student.online_resume }}</h3>
+    <h3>photo: {{ student.photo }}</h3>
     <router-link v-bind:to="`/`">Home</router-link>
   </div>
 </template>
