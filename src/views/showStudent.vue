7f7614a7a13071e2ed33b022aef679cad0c3a483
Ian Boyle <irboyle21@gmail.com>
2021-12-08 15:29:51 -0500

    gets rid of placeholder twitter link

--- src/views/showStudent.vue
+++ src/views/showStudent.vue
@@ -23,7 +23,7 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      student: { twitter: "Snowden" },
+      student: {},
     };
   },
   created: function () {
