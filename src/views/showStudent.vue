b4f6a3386e1a22dde7abae10b219d2ee930d77c0
Ian Boyle <irboyle21@gmail.com>
2021-12-08 16:25:56 -0500

    adds links for linkedin, personal website to showStudent page

--- src/views/showStudent.vue
+++ src/views/showStudent.vue
@@ -5,12 +5,18 @@
     <h3>email:{{ student.email }}</h3>
     <h3>Phone number: {{ student.phone_number }}</h3>
     <h3>Short bio: {{ student.short_bio }}</h3>
-    <h3>Linkedin: {{ student.linkedin }}</h3>
+    <h3>
+      Linkedin:
+      <a v-bind:href="`${student.linkedin}`">{{ student.linkedin }}</a>
+    </h3>
     <h3>
       Twitter handle:
       <a v-bind:href="`https://twitter.com/${student.twitter}`">{{ student.twitter }}</a>
     </h3>
-    <h3>Website: {{ student.website }}</h3>
+    <h3>
+      Website:
+      <a v-bind:href="`${student.website}`">{{ student.website }}</a>
+    </h3>
     <h3>Online resume: {{ student.online_resume }}</h3>
     <h3>photo: {{ student.photo }}</h3>
     <h3>Skills:</h3>
