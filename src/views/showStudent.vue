6734b5b3333ed5f50460e6e59ed8ce0f303402a0
Ian S <iansilber32@gmail.com>
2021-12-08 14:07:58 -0500

    creates basic show page for students

---
+++ src/views/showStudent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="students-show">
+    <h1>First name: {{ student.first_name }}</h1>
+    <h3>email:{{ student.email }}</h3>
+    <h3>Twitter handle: {{ student.twitter_handle }}</h3>
+    <router-link v-bind:to="`/`">Home</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      student: {},
+    };
+  },
+  created: function () {
+    axios.get("/students/" + this.$route.params.id).then((response) => {
+      this.student = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
